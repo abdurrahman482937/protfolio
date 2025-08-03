ed2c83e9fcaf563ee68ec30a20d1ab39b5fc2743
abdurrahman482937 <abdurrahmanass2024@gmail.com>
2025-08-03 14:36:06 +0600

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
+import Header from './components/Header.jsx';
 
 const App = () => {
     return (
-        <div>
-            Hello
-        </div>
+        <Header />
     );
 };
 
